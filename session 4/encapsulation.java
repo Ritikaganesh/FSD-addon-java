5cf0cd53787fccf6a033e2526ddd6c4b5c84fb85
Ritikaganesh <ritika.gk07@gmail.com>
2025-03-24 11:33:12 +0530

    encapsulation.java

--- session 4/encapsulation.java
+++ session 4/encapsulation.java
@@ -4,6 +4,18 @@ public class encapsulation {
 	
 	private String name;
 	private String location;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
 	
 
 }
